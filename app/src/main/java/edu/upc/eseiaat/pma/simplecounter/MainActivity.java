285d222ba0f5192892756ecedf3da708726c382a
AlejandroJG <ajaimezg@hotmail.com>
2017-11-01 11:32:09 +0100

    SimpleCounter Ya funciona

--- app/src/main/java/edu/upc/eseiaat/pma/simplecounter/MainActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/simplecounter/MainActivity.java
@@ -1,17 +1,19 @@
 package edu.upc.eseiaat.pma.simplecounter;
 
 import android.content.DialogInterface;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+private int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +23,28 @@ public class MainActivity extends AppCompatActivity {
         Button BOTONSUMAR = (Button) findViewById(R.id.Botonsumar);
         Button BOTONRESTAR= (Button) findViewById(R.id.Botonrestar);
         Button BOTONACABAR= (Button) findViewById(R.id.Botonacabar);
-        final EditText number = (EditText) findViewById(R.id.editTextNumber);
+        final TextView number = (TextView) findViewById(R.id.textView2);
+        contador=0;
 
 
         BOTONSUMAR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String numero1 = number.getText().toString();
-                int num1 = Integer.parseInt(numero1);
-                num1++;
-                String snum = String.format("%f", num1);
-                number.setText(snum);
+                contador++;
+                String count = String.format("%s",contador );
+                number.setText(count);
+
 
             }
         });
         BOTONRESTAR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String numero2 = number.getText().toString();
-                int num = Integer.parseInt(numero2);
-                if(num>0){num--;}
-                else num=0;
-                String snum = String.format("%f", num);
-                number.setText(snum);
+                if(contador>0){contador--;}
+                else contador=0;
+                String count1 = String.format("%s",contador );
+                number.setText(count1);
+
             }
         });
 
